ec6a37ac354dae004e403b31c68461e19b35db16
rkorzen <korzeniewski@gmail.com>
2025-02-21 14:07:31 +0100

    domkniecie cwiczenie

---
+++ cwiczenia/cwiczenie_domkniecie.py
@@ -0,0 +1,16 @@
+"""
+
+Napisz funkcje id_generator(prefix), która zwróci funkcję, generująca kolejne unikalne identyfukatory
+
+
+user_id = id_genrator("user_")
+print(user_id()) # user_1
+print(user_id()) # user_2
+print(user_id()) # user_3
+
+order_id = id_genrator("order_")
+print(order_id()) # order_1
+print(order_id()) # order_2
+print(order_id()) # order_3
+
+"""
--- cwiczenia/kalkulator.py
+++ cwiczenia/kalkulator.py
@@ -22,8 +22,63 @@ lista = [add, sub]
 
 for func in lista:
     func(1, 2)
-
-
-
 """
+import logging
+
+
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename="logs.log")
+logger = logging.getLogger(__name__)
+
+
+
+def add(a, b):
+    logger.debug("Wywołano funkcję add z arg %s %s" % (a, b) )
+    return a + b
+
+def sub(a, b):
+    logger.debug("Wywołano funkcję sub z arg %s %s" % (a, b))
+    return a - b
+
+def mul(a, b):
+    logger.debug("Wywołano funkcję mul z arg %s %s" % (a, b))
+    return a * b
+
+def div(a, b):
+    logger.debug("Wywołano funkcję div z arg %s %s" % (a, b))
+    if b == 0:
+        logger.warning("Wywołano funkcję div dzielnikiem 0! Sprawdz dane!!" )
+        raise ValueError("Dzielnik nie może być 0")
+    return a / b
+
+def power(a, b):
+    logger.debug("Wywołano funkcję div z arg %s %s" % (a, b))
+    return a ** b
+
+def get_data():
+    op = input("Podaj rodzaj operacji: (+-*/^)")
+    a = float(input("Podaj arg 1: "))
+    b = float(input("Podaj arg 2: "))
+    logger.debug("Pobrano dane %s %s %s" % (op, a, b))
+    return op, a, b
+
+
+def main():
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div,
+        "^": power
+
+    }
+
+    op, a, b = get_data()
+
+    result = operations[op](a, b)
+
+    print(f"Wynik operacji ({op}) na arg: ({a}, {b}) to: {result}")
+
+
+if __name__ == "__main__":
+    main()
 
