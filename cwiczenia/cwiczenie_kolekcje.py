321335729f59aa420233509ad8ec3b4517b31045
rkorzen <korzeniewski@gmail.com>
2025-02-21 10:38:30 +0100

    cwiczenie

--- cwiczenia/cwiczenie_kolekcje.py
+++ cwiczenia/cwiczenie_kolekcje.py
@@ -25,3 +25,46 @@ przydatne rzeczy:
 """
 
 
+def get_data():
+    print("Podawaj liczbe i nacisnij enter lub wcisnij enter bez podawania niczego by zakonczyc")
+
+    data = []
+
+    while True:
+        answer = input("Podaj liczbę: ")
+        if answer == "":
+            break
+        data.append(int(answer))
+    return data
+
+def get_len_of_unique_elements(data): return len(set(data))
+
+def get_len_of_even_numbers(data):
+    filtered = [d for d in data if d % 2 == 0]
+    return len(filtered)
+
+def get_len_of_odd_numbers_from_range(data, start=1, stop=101):
+    odd_in_range = {x for x in range(start, stop, 2)}
+    return len(set(data) & odd_in_range)
+
+def get_mean(data):
+    return sum(data) / len(data)
+
+def main():
+    data = get_data()
+
+    print(f"Unikalnych liczb: {get_len_of_unique_elements(data)}")
+    print(f"Parzystych liczb: {get_len_of_even_numbers(data)}")
+    print(f"Nieparzystch liczb z zakresu (1-101): {get_len_of_odd_numbers_from_range(data)}")
+    print(f"Suma liczb: {sum(data)}")
+    print(f"Srednia: {get_mean(data)}")
+
+
+# print(dir())
+# print(__name__)
+
+if __name__ == "__main__":
+    print("Cos tam")
+    main()
+
+Przerwa do 10:52
---
+++ cwiczenia/korzystanie_z_funkcji_z_cwiczenie_kolekcja.py
@@ -0,0 +1,3 @@
+from cwiczenie_kolekcje import get_mean
+
+print(get_mean([1, 2, 3]))
