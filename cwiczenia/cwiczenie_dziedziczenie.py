11ca2f9e26960c13bafa17730a25d15fd61c236a
rkorzen <korzeniewski@gmail.com>
2025-03-04 12:28:18 +0100

    zwyklu pracownik

--- cwiczenia/cwiczenie_dziedziczenie.py
+++ cwiczenia/cwiczenie_dziedziczenie.py
@@ -18,11 +18,9 @@ Pracownik premiun:     EmployeePremium
 
 Pracownik przedstawia sie
 
-
-
 >>> e = Employee("Rafał", rate_per_hour=100)
 >>> e.introduce()
-"Jestem Rafał, moja stawka to 100/h, nierozliczone godziny s:0 n:0 t:0
+'Jestem Rafał, moja stawka to 100/h, nierozliczone godziny s:0 n:0 t:0'
 >>> e.pay_salary()
 0
 >>> e.register_time(5)
@@ -32,9 +30,11 @@ Pracownik przedstawia sie
 0
 >>> e.register_time(10)
 >>> e.introduce()
-"Jestem Rafał, moja stawka to 100/h, nierozliczone godziny s:8 n:2 t:10
+'Jestem Rafał, moja stawka to 100/h, nierozliczone godziny s:8 n:2 t:10'
 >>> e.pay_salary()
 1200
+>>> e.introduce()
+'Jestem Rafał, moja stawka to 100/h, nierozliczone godziny s:0 n:0 t:0'
 
 Pracownik Premium ma mozliwosc otrzymania bonusa
 
@@ -43,5 +43,35 @@ Pracownik Premium ma mozliwosc otrzymania bonusa
 >>> pe.add_bonus(1000)
 >>> pe.pay_salary()
 1500
+>>> pe.pay_salary()
+0
 
 """
+
+class Employee:
+
+    def __init__(self, name: str, rate_per_hour: int):
+        self.name = name
+        self.rate_per_hour = rate_per_hour
+
+        self.hours: int = 0
+        self.over_hours: int = 0
+        self.total_hours: int = 0
+
+
+    def introduce(self) -> str:
+        return (f"Jestem {self.name}, moja stawka to {self.rate_per_hour}/h, "
+                f"nierozliczone godziny s:{self.hours} n:{self.over_hours} t:{self.over_hours + self.hours}")
+
+    def pay_salary(self) -> int:
+        to_pay = self.hours * self.rate_per_hour + self.over_hours * self.rate_per_hour * 2
+        self.hours = 0
+        self.over_hours = 0
+        return to_pay
+
+    def register_time(self, hours: int) -> None:
+        if hours <= 8:
+            self.hours = hours
+        else:
+            self.hours = 8
+            self.over_hours = hours - 8
