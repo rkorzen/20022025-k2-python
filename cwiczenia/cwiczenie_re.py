46c8c8e724b08c091fd523d49a255426941c25da
rkorzen <korzeniewski@gmail.com>
2025-03-06 09:51:56 +0100

    wyrazenia regularne cwiczenie rozwiazanie

--- cwiczenia/cwiczenie_re.py
+++ cwiczenia/cwiczenie_re.py
@@ -7,3 +7,16 @@ Z pliku dane/dane.txt wybierz przy pomocy wyrazen regularnych:
 
 wypisz każda listę na ekranie
 """
+
+import re
+
+ip_pattern = re.compile(r"(?:\d{1,3}\.){3}\d{1,3}")
+date_pattern = re.compile(r"\d{4}-\d{2}-\d{2}")
+email_pattern = re.compile(r"\w+@\w+\.\w{3}")
+
+with open("dane/dane.txt") as f:
+    text = f.read()
+
+print(ip_pattern.findall(text))
+print(date_pattern.findall(text))
+print(email_pattern.findall(text))
