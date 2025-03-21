0a945da602f4946677b1ae930d4cc7ac2c45f313
rkorzen <korzeniewski@gmail.com>
2025-03-21 11:24:13 +0100

    dodanie widokow api w books

--- projekty/spinoza/src/books/api_views.py
+++ projekty/spinoza/src/books/api_views.py
@@ -1,6 +1,38 @@
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from .models import Genre, Author
+from .serializers import GenreSerializer, AuthorSerializer
+
+@csrf_exempt
+def genre_list(request):
+
+    if request.method == "GET":
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = GenreSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
-def genre_list(request): ...
+@csrf_exempt
+def author_list(request):
 
-
-def author_list(request): ...
+    if request.method == "GET":
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = AuthorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
